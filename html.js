16149213f8b1dcdd5b1543b02c0a4a12bb34c78b
brandly <brandly13@gmail.com>
2020-03-09 23:09:42 -0400

    better markup

--- html.js
+++ html.js
@@ -4,16 +4,19 @@ const list = fs
   .readFileSync('./list.txt', 'utf-8')
   .trim()
   .split('\n')
-  .map(line => line.split(' - '))
+  .map(line => line.trim().split(' - '))
 
 const search = q => `https://google.com/search?q=${encodeURIComponent(q)}`
 const link = v => `<a href="${search(v)}">${v}</a>`
 
-console.log(`<style>
-ul { list-style-type: none; padding: 0 }
+console.log(`<!doctype html>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+<style>
+ul { list-style-type: none; padding: 0; }
 li { margin: 6px 0; }
-</style`)
-console.log(`<body>
+</style>
+<body>
 <ul>
 ${list
   .map(([school, player]) => `<li>${link(school)}<br/>${link(player)}</li>`)
